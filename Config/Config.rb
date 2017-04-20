0c7b24a7daa1867d86b4d933831a360ef421620a
Lynx <titshacking@users.noreply.github.com>
2017-04-20 06:17:15 +0530

    Create Config.rb

---
+++ Config/Config.rb
@@ -0,0 +1,10 @@
+def getConfig
+	config = {
+		'server_port' => 6112,
+		'database_user' => 'root',
+		'database_pass' => '1337h4x0r',
+		'database_host' => '127.0.0.1',
+		'database_name' => 'purecp'
+	}
+	return config
+end
