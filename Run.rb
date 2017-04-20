be6242665213506f6347d9b0228bc31c1d4595e6
Lynx <titshacking@users.noreply.github.com>
2017-04-20 06:18:40 +0530

    Create Run.rb

---
+++ Run.rb
@@ -0,0 +1,20 @@
+require './Config/Config.rb'
+require './Extensions/Crumbs.rb'
+require './Extensions/Database.rb'
+require './Extensions/TCP.rb'
+require './Extensions/XTParser.rb'
+
+require './Core/Login.rb'
+require './Core/Game.rb'
+
+require './Core/ClubPenguin.rb'
+require './Core/CPUser.rb'
+
+
+server = ClubPenguin.new(getConfig)
+
+server.startEmulator
+
+while true
+	server.sock.listenServer
+end
