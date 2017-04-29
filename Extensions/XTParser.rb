cddc9684905226ed9e545f6652ee50c8246260de
Lynx <titshacking@users.noreply.github.com>
2017-04-29 12:55:19 +0530

    Load list of packets through JSON

--- Extensions/XTParser.rb
+++ Extensions/XTParser.rb
@@ -1,212 +1,55 @@
-class XTParser
+require 'rubygems'
+require 'json'
 
-	XTPACKETS = Hash[
-		'xt' => [
-			's' => [
-				'delimeter' => '#',
-				'j#js' => [
-					'method' => 'handleJoinServer', 
-					'args' => ['id', 'loginkey', 'lang'], 
-					'type' => ['Fixnum', 'String', 'String'], 
-					'length' => 2, 
-					'hasException' => false
-				],
-				'j#jp' => [
-					'method' => 'handleJoinPlayer', 
-					'args' => ['room_id'], 
-					'type' => ['Fixnum'], 
-					'length' => 0, 
-					'hasException' => false
-				],
-				'j#jg' => [
-					'method' => 'handleJoinGame', 
-					'args' => ['room_id'], 
-					'type' => ['Fixnum'], 
-					'length' => 0, 
-					'hasException' => false
-				],
-				'j#grs' => [
-					'method' => 'handleGetRoomSynced', 
-					'args' => [], 
-					'type' => [],
-					'length' => -1, 
-					'hasException' => false
-				],
-				'j#jr' => [
-					'method' => 'handleJoinRoom', 
-					'args' => ['room_id', 'x', 'y'], 
-					'type' => ['Fixnum', 'Fixnum', 'Fixnum'], 
-					'length' => 2, 
-					'hasException' => false
-				],
-				'i#gi' => [
-					'method' => 'handleGetInventory', 
-					'args' => [], 
-					'type' => [],
-					'length' => -1, 
-					'hasException' => false
-				],
-				'f#epfgf' => [
-					'method' => 'handleEPFGetField', 
-					'args' => [], 
-					'type' => [], 
-					'length' => -1, 
-					'hasException' => false
-				],
-				'g#ur' => [
-					'method' => 'handleGetFurnitureRevision', 
-					'args' => [], 
-					'type' => [], 
-					'length' => -1, 
-					'hasException' => true
-				],
-				'm#sm' => [
-					'method' => 'handleSendMessage', 
-					'args' => ['message'], 
-					'type' => ['String'], 
-					'length' => 0, 
-					'hasException' => true
-				],
-				'st#ssbcd' => [
-					'method' => 'handleSetStampBookCoverDetails', 
-					'args' => [],
-					'type' => [], 
-					'length' => 4, 
-					'hasException' => true
-				],
-				'b#gb' => [
-					'method' => 'handleGetBuddies',
-					'args' => [],
-					'type' => [],
-					'length' => -1,
-					'hasException' => false
-				],
-				'n#gn' => [
-					'method' => 'handleGetIgnored',
-					'args' => [],
-					'type' => [],
-					'length' => -1,
-					'hasException' => false
-				],
-				'l#mst' => [
-					'method' => 'handleMailStart',
-					'args' => [],
-					'type' => [],
-					'length' => -1,
-					'hasException' => false
-				],
-				'l#mg' => [
-					'method' => 'handleMailGet',
-					'args' => [],
-					'type' => [],
-					'length' => -1,
-					'hasException' => false
-				],
-				'p#pgu' => [
-					'method' => 'handlePuffleGetUser',
-					'args' => [],
-					'type' => [],
-					'length' => -1,
-					'hasException' => false
-				],
-				'u#glr' => [
-					'method' => 'handleGetLatestRevision',
-					'args' => [],
-					'type' => [],
-					'length' => -1,
-					'hasException' => false
-				],
-				'f#efpga' => [
-					'method' => 'handleEPFGetAgent',
-					'args' => [],
-					'type' => [],
-					'length' => -1,
-					'hasException' => false
-				],
-				'f#efpgr' => [
-					'method' => 'handleEPFGetRevision',
-					'args' => [],
-					'type' => [],
-					'length' => -1,
-					'hasException' => false
-				],
-				'u#h' => [
-					'method' => 'handleUserHeartbeat',
-					'args' => [],
-					'type' => [],
-					'length' => -1,
-					'hasException' => false
-				],
-				'u#sp' => [
-					'method' => 'handleSendPosition',
-					'args' => ['xaxis', 'yaxis'],
-					'type' => ['Fixnum', 'Fixnum'],
-					'length' => 1,
-					'hasException' => false
-				],
-				'u#sf' => [
-					'method' => 'handleSendFrame',
-					'args' => ['frame'],
-					'type' => ['Fixnum'],
-					'length' => 0,
-					'hasException' => false
-				]
-			],
-			'z' => [
-				'jw' => [
-					'method' => [
-						'handleJoinWaddle', 
-						'args' => ['waddle_id'], 
-						'type' => ['Fixnum'], 
-						'length' => 0, 
-						'hasException' => false
-					]
-				]
-			]
-		]
-	]
+class XTParser
+	
+	def handleLoadPackets
+		packet_file = File.read(__dir__ + '/PACKETS.json')
+		packets = JSON.parse(packet_file)
+		@xtPackets = packets
+	end
 	
 	def parseData(data)
 		if data.respond_to?(:to_str)
 			packets = data.split('%')
-			if XTPACKETS.has_key?(packets[1]) != true
+			if @xtPackets.has_key?(packets[1]) != true
 				@parent.logger.warn('Client is trying to send an invalid packet')
 				return false
 			end
-			if XTPACKETS[packets[1]][0].include?(packets[2]) != true
+			if @xtPackets[packets[1]][0].include?(packets[2]) != true
 				@parent.logger.warn('Client is trying to send invalid packet type')
 				return false
 			end
 			gamePacket = packets[3]
-			existsDelimeter = XTPACKETS[packets[1]][0][packets[2]][0]['delimeter']
+			existsDelimeter = @xtPackets[packets[1]][0][packets[2]][0]['delimeter']
 			if gamePacket !~ /#{existsDelimeter}/
 				@parent.logger.warn('Client is trying to use an invalid delimeter')
 				return false
 			end
-			if XTPACKETS[packets[1]][0][packets[2]][0].include?(gamePacket) != true
+			if @xtPackets[packets[1]][0][packets[2]][0].include?(gamePacket) != true
 				@parent.logger.warn('Client is trying to send an invalid game packet')
 				return false
 			end
 			packets.each do |packet|
-				if packet.include?('|') && XTPACKETS[packets[1]][0][packets[2]][0][gamePacket][0]['hasException'] != true
+				if packet.include?('|') && @xtPackets[packets[1]][0][packets[2]][0][gamePacket][0]['hasException'] != true
 					@parent.logger.warn('Client is trying to send a malformed packet')
 					return false
 				end
 			end
 			realArgs = packets.drop(5)
-			if realArgs.empty? != true && realArgs.count < (XTPACKETS[packets[1]][0][packets[2]][0][gamePacket][0]['length'] - 1)
+			if realArgs.empty? != true && realArgs.count < (@xtPackets[packets[1]][0][packets[2]][0][gamePacket][0]['length'] - 1)
 				@parent.logger.warn('Client is sending invalid amount of Arguments')
 				return false
 			end
-			if XTPACKETS[packets[1]][0][packets[2]][0][gamePacket][0]['length'] < 0
-				handlingInfo = ['handler' => XTPACKETS[packets[1]][0][packets[2]][0][gamePacket][0]['method'], 'arguments' => []]
+			if @xtPackets[packets[1]][0][packets[2]][0][gamePacket][0]['length'] < 0
+				handlingInfo = ['handler' => @xtPackets[packets[1]][0][packets[2]][0][gamePacket][0]['method'], 'arguments' => []]
 				return handlingInfo
-			elsif XTPACKETS[packets[1]][0][packets[2]][0][gamePacket][0]['length'] >= 0
-				packLength = XTPACKETS[packets[1]][0][packets[2]][0][gamePacket][0]['length']
+			elsif @xtPackets[packets[1]][0][packets[2]][0][gamePacket][0]['length'] >= 0
+				packLength = @xtPackets[packets[1]][0][packets[2]][0][gamePacket][0]['length']
 				newArgs = Array.new
 				(0..packLength).each do |pack_index|
-					name = XTPACKETS[packets[1]][0][packets[2]][0][gamePacket][0]['args'][pack_index]
-					type = XTPACKETS[packets[1]][0][packets[2]][0][gamePacket][0]['type'][pack_index]
+					name = @xtPackets[packets[1]][0][packets[2]][0][gamePacket][0]['args'][pack_index]
+					type = @xtPackets[packets[1]][0][packets[2]][0][gamePacket][0]['type'][pack_index]
 					item = realArgs[pack_index]
 					item_type = ''
 					if @parent.is_num?(item) == true
@@ -222,7 +65,7 @@ class XTParser
 					end
 					newArgs.push(item)
 				end
-				handlingInfo = ['handler' => XTPACKETS[packets[1]][0][packets[2]][0][gamePacket][0]['method'], 'arguments' => newArgs]
+				handlingInfo = ['handler' => @xtPackets[packets[1]][0][packets[2]][0][gamePacket][0]['method'], 'arguments' => newArgs]
 				return handlingInfo
 			end
 		end
