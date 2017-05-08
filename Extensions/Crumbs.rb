ff300f6634a57f4aaa8427f56903d8842e27f350
Lynx <titshacking@users.noreply.github.com>
2017-05-08 06:21:33 +0530

    Types assignment

--- Extensions/Crumbs.rb
+++ Extensions/Crumbs.rb
@@ -10,13 +10,13 @@ class Crumbs
 	def initialize(main_class)
 		@parent = main_class
 		@urls = {
-			'rooms' => 'http://127.0.0.1/website/JSONS/rooms.json',
-			'stamps' => 'http://127.0.0.1/website/JSONS/stamps.json',
-			'postcards' => 'http://127.0.0.1/website/JSONS/postcards.json',
-			'items' => 'http://127.0.0.1/website/JSONS/paper_items.json',
-			'igloos' => 'http://127.0.0.1/website/JSONS/igloos.json',
-			'floors' => 'http://127.0.0.1/website/JSONS/igloo_floors.json',
-			'furniture' => 'http://127.0.0.1/website/JSONS/furniture_items.json'
+			'rooms' => 'http://127.0.0.1/JSONS/rooms.json',
+			'stamps' => 'http://127.0.0.1/JSONS/stamps.json',
+			'postcards' => 'http://127.0.0.1/JSONS/postcards.json',
+			'items' => 'http://127.0.0.1/JSONS/paper_items.json',
+			'igloos' => 'http://127.0.0.1/JSONS/igloos.json',
+			'floors' => 'http://127.0.0.1/JSONS/igloo_floors.json',
+			'furniture' => 'http://127.0.0.1/JSONS/furniture_items.json'
 		}	
 		@item_crumbs = Hash.new{ |h,k| h[k] = Hash.new(&h.default_proc) }
 		@epf_item_crumbs = Hash.new{ |h,k| h[k] = Hash.new(&h.default_proc) }
@@ -68,9 +68,32 @@ class Crumbs
 		decoded_items_data = JSON.parse(crumbs_data)
 		decoded_items_data.each do |item|
 			if item['is_epf'].to_bool == true
-				@epf_item_crumbs[item['paper_item_id']] = ['points' => item['cost']]
+				@epf_item_crumbs[item['paper_item_id'].to_i] = ['points' => item['cost'].to_i]
 			else 
-				@item_crumbs[item['paper_item_id']] = ['price' => item['cost']]
+				item_type = ''
+				case item['type']
+					when 1 
+						item_type = 'color'
+					when 2 
+						item_type = 'head'
+					when 3 
+						item_type = 'face'
+					when 4 
+						item_type = 'neck'
+					when 5 
+						item_type = 'body'
+					when 6 
+						item_type = 'hand'
+					when 7 
+						item_type = 'feet'
+					when 8 
+						item_type = 'flag'
+					when 9 
+						item_type = 'photo'
+					when 10 
+						item_type = 'other'				
+				end
+				@item_crumbs[item['paper_item_id'].to_i] = ['price' => item['cost'].to_i, 'type' => item_type.to_i]
 			end
 		end	
 		@parent.logger.info('Successfully loaded ' + @epf_item_crumbs.count.to_s + ' EPF Items')
@@ -80,7 +103,7 @@ class Crumbs
 	def loadFloors(crumbs_data)
 		decoded_floors_data = JSON.parse(crumbs_data)
 		decoded_floors_data.each do |floor|
-			@floors_crumbs[floor['igloo_floor_id']] = ['price' => floor['cost']]	
+			@floors_crumbs[floor['igloo_floor_id'].to_i] = ['price' => floor['cost'].to_i]	
 		end
 		@parent.logger.info('Successfully loaded ' + @floors_crumbs.count.to_s + ' Floors')
 	end
@@ -91,7 +114,7 @@ class Crumbs
 			first_stamps_index.each do |value_two, value_three|
 				if value_three.respond_to?('each')
 					value_three.each do |stamps|
-						@stamps_crumbs[stamps['stamp_id']] = ['rank' => stamps['rank']]
+						@stamps_crumbs[stamps['stamp_id'].to_i] = ['rank' => stamps['rank'].to_i]
 					end
 				end
 			end
@@ -102,7 +125,7 @@ class Crumbs
 	def loadPostcards(crumbs_data)
 		decoded_pcards_data = JSON.parse(crumbs_data)
 		decoded_pcards_data.each do |card_id, card_cost|
-			@postcard_crumbs[card_id] = ['cost' => card_cost]
+			@postcard_crumbs[card_id.to_i] = ['cost' => card_cost.to_i]
 		end
 		@parent.logger.info('Successfully loaded ' + @postcard_crumbs.count.to_s + ' Postcards')
 	end
@@ -110,7 +133,7 @@ class Crumbs
 	def loadIgloos(crumbs_data)
 		decoded_igloos_data = JSON.parse(crumbs_data)
 		decoded_igloos_data.each do |igloo_index, igloo|
-			@igloo_crumbs[igloo['igloo_id']] = ['price' => igloo['cost']]	
+			@igloo_crumbs[igloo['igloo_id'].to_i] = ['price' => igloo['cost'].to_i]	
 		end
 		@parent.logger.info('Successfully loaded ' + @igloo_crumbs.count.to_s + ' Igloos')
 	end
@@ -118,7 +141,7 @@ class Crumbs
 	def loadFurnitures(crumbs_data)
 		decoded_furns_data = JSON.parse(crumbs_data)
 		decoded_furns_data.each do |furn|
-			@furniture_crumbs[furn['furniture_item_id']] = ['price' => furn['cost']]	
+			@furniture_crumbs[furn['furniture_item_id'].to_i] = ['price' => furn['cost'].to_i]	
 		end
 		@parent.logger.info('Successfully loaded ' + @furniture_crumbs.count.to_s + ' Furnitures')
 	end
@@ -128,9 +151,9 @@ class Crumbs
 		decoded_rooms_data.each do |room_index|
 			room_index.each do |room|
 				if room['room_key'] != ''
-					@room_crumbs[room['room_id']] = ['name' => room['room_key'], 'max' => room['max_users']]	
+					@room_crumbs[room['room_id'].to_i] = ['name' => room['room_key'], 'max' => room['max_users'].to_i]	
 				else
-					@game_room_crumbs[room['room_id']] = ['max' => room['max_users']]
+					@game_room_crumbs[room['room_id'].to_i] = ['max' => room['max_users'].to_i]
 				end
 			end
 		end
