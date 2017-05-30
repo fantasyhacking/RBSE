51456a297222985cf4a525aa24a6b050b730ca84
Lynx <titshacking@users.noreply.github.com>
2017-05-30 05:49:58 +0530

    Create config.php

---
+++ Register/config.php
@@ -0,0 +1,18 @@
+<?php
+
+$arrConfig = array(
+			'secret_key' => '6LcelR8TAAAAAAQ8wBgZSlrcd4hNI2sQ2FrUhQRl',
+			'database' => 'purecp',
+			'username' => 'root',
+			'password' => '1337h4x0r',
+			'host' => '127.0.0.1', #This always remains localhost
+			'tables' => array(
+					1 => 'users', 
+					2 => 'igloos',
+					3 => 'postcards',
+					4 => 'stamps',
+					5 => 'epf'
+			)
+);
+
+?>
