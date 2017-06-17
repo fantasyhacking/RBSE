3a7cf6c4884dc728b9b562653e1021703e4ca2cf
Lynx <titshacking@users.noreply.github.com>
2017-06-17 06:32:16 +0530

    Added loading of hooks - sorry I forgot

--- Core/ClubPenguin.rb
+++ Core/ClubPenguin.rb
@@ -4,8 +4,8 @@ require 'time'
 require 'nokogiri'
 
 class ClubPenguin
-
-	attr_accessor :logger, :crumbs, :server_config, :mysql, :sock, :login_sys, :game_sys, :crypto
+	
+	attr_accessor :logger, :crumbs, :server_config, :mysql, :sock, :login_sys, :game_sys, :crypto, :hooks
 
 	def initialize(config_info)
 		@server_config = config_info
@@ -17,6 +17,7 @@ class ClubPenguin
 		@sock = TCP.new(self)
 		@login_sys = Login.new(self)
 		@game_sys = Game.new(self)
+		@hooks = Hash.new
 	end
 	
 	def startEmulator
@@ -24,6 +25,7 @@ class ClubPenguin
 		@crumbs.loadCrumbs
 		@mysql.connectMySQL(@server_config['database_user'], @server_config['database_pass'], @server_config['database_host'], @server_config['database_name'])
 		@sock.connectServer
+		self.loadHooks
 	end
 
 	def displayASCII
@@ -36,6 +38,35 @@ class ClubPenguin
 		@logger.info('Created by: Lynx')
 		@logger.info('Protocol: Actionscript 2.0')
 		@logger.info("LICENSE: MIT\n")
+	end
+	
+	def loadHooks
+		Dir[Dir.pwd + "/Hooks/*.rb"].each { |hook_file| require hook_file }
+		Dir[Dir.pwd + "/Hooks/*.rb"].each do |hook|
+			hookName = File.basename(hook, ".rb")
+			hookClass =  hookName.constantize
+			@hooks[hookName] = hookClass.new(self)
+		end
+		@hooks.each do |hookName, hookClass|
+			if hookClass.enabled == false
+				@hooks.delete(hookName)
+			end
+			if hookClass.dependencies.empty? == true
+				return @logger.info("Dependencies for hook: #{hookName} is empty!")
+			elsif hookClass.dependencies.count < 3
+				return @logger.info("Missing dependencies in hook: #{hookName}")
+			elsif hookClass.dependencies.count > 3
+				return @logger.info("Too many dependencies in hook: #{hookName}")
+			elsif hookClass.dependencies['author'] == '' || hookClass.dependencies['version'] == '' || hookClass.dependencies['plugin_type'] == ''
+				return @logger.info("One of the dependencies is not defined for hook: #{hookName}")
+			end
+		end
+		hooksLoaded = @hooks.count
+		if hooksLoaded == 0
+			@logger.info('No hooks loaded at the moment')
+		else
+			@logger.info("Successfully loaded #{hooksLoaded} hooks")
+		end
 	end
 	
 	def parseXML(data)
@@ -58,4 +89,6 @@ class ClubPenguin
 		return randString
 	end
 	
+	
 end
+\
